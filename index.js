833d95ff71401ff21e36b5653be02c06646e7604
SchinoTheRapper <110900056+Moschifire@users.noreply.github.com>
2023-01-25 20:09:32 +0100

    till next edit

--- index.js
+++ index.js
@@ -31,9 +31,37 @@ function App() {
                     </div>
                     <div className="project">
                         <h4>Drum Machine</h4>
-                        <p></p>
+                        <p>This drum machine was created with react. When a key is clicked, it plays a sound which is recorded and can be replayed. The speed of play as well as the volume can be adjusted.</p>
                         <a href="https://drum-machine-nu-ruddy.vercel.app/" className="btn btn-primary" target="_blank">Click here to visit</a>
                     </div>
+                    <div className="project">
+                        <h4>JavaScript React Calculator</h4>
+                        <p>React framework was used to create this calculator. It performs basic operations which are addition, subtraction, multiplication and division.</p>
+                        <a href="https://javascript-react-calc.vercel.app/" className="btn btn-primary" target="_blank">Click here to visit</a>
+                    </div>
+                    <div className="project">
+                        <h4>Pomodoro Clock</h4>
+                        <p>Also known as a 25 + 5 clock, this clock that was created with react has 5 breaks each with a session of 25 minutes. At each break, a beep alerts the user. It can be paused and reset.</p>
+                        <a href="https://pomodoro-clock-sigma.vercel.app/" className="btn btn-primary" target="_blank">Click here to visit</a>
+                    </div>
+                    <div className="project">
+                        <h4>Feed-A-Mole Game</h4>
+                        <p>Instead of whacking the moles, the moles are fed in this game. It was created with HTML, CSS and JavaScript. The moles are fed or they return sad.</p>
+                        <a href="https://mole-game-rose.vercel.app/" className="btn btn-primary" target="_blank">Click here to visit</a>
+                    </div>
+                </div>
+            </div>
+            <div id="contact">
+                <h2>Contact Me</h2>
+                <div id="location">
+                    <p><i class="fa-solid fa-location-dot"></i> Lagos, Nigeria</p>
+                    <p><i class="fa-solid fa-envelope"></i> markoladeinde@gmail.com</p>
+                </div>
+                <div id="social">
+                    <a id="icon" href="https://twitter.com/SchinoTheRapper" className="btn" target="_blank"><i class="fa-brands fa-twitter"></i></a>
+                    <a id="icon" href="https://github.com/Moschifire" className="btn" target="_blank"><i class="fa-brands fa-github"></i></a>
+                    <a id="icon" href="https://www.linkedin.com/in/mark-oladeinde-aa21271b2/" className="btn" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
+                    <a id="icon" href="https://drive.google.com/file/d/1Nik9tLLAsWrL4RoBpHce3L2fq2naQbO4/view?usp=sharing" className="btn" target="_blank"><i class="fa-solid fa-file"></i></a>
                 </div>
             </div>
         </div>
